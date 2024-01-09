26a0af060db64990d0d86139182d4433a40e5ddb
kschipper <kate.schipper@unil.ch>
2024-01-09 16:06:27 +0100

    commit of cfg file

---
+++ cfg_coregistration.m
@@ -0,0 +1,16 @@
+%% BEAM LAB Unil 2024
+
+% Paths used by the matlab script. User needs to enter the information
+% below once before running step1 to step4 
+
+% Authors:
+% Kate Schipper
+% Paolo Ruggeri 
+
+% root path
+root_path = 'C:\Users\kschippe\OneDrive\Documents\UNIL\Research\EEG\'; % where coregistration_process_final is placed
+
+% database path
+database_path_root = 'brainstorm_db'; % brainstorm database folder: should remain like this
+database_name = 'coregistration_process_final'; % our project folder name
+database_path = [database_path_root filesep database_name];
